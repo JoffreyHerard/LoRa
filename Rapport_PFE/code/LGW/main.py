5d12a496858f0b074e663895e6bd10982bbc7caf
Apocalypzer <apocalypzer@gmail.com>
2018-03-24 14:24:19 +0100

    maj rapport

--- Rapport_PFE/code/LGW/main.py
+++ Rapport_PFE/code/LGW/main.py
@@ -4,7 +4,16 @@ while True:
         pycom.rgbled(0x007f00) # green
         data = s.recv(128)
         handle_message(data)
-        standard()
+        time.sleep(0.500)
+        recolte=standard()
+        print(recolte)
+        time.sleep(0.500)
+        if recolte !="" :
+            changetoLW()
+            s.setblocking(True)
+            send_datatoLWGW(s,recolte)
+            s.setblocking(False)
+            changetoLoRa(lora)
     else:
         pycom.rgbled(0x7f0000) #red
         print("I am sleeping")
@@ -12,3 +21,4 @@ while True:
         isListening=True
         del clock
         clock = TimerL(slot)
+
