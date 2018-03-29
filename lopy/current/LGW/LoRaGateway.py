61c62bc36d396994147f33bb777f5c789543212d
Apocalypzer <apocalypzer@gmail.com>
2018-03-29 14:14:12 +0200

    Simulation + study case

--- lopy/current/LGW/LoRaGateway.py
+++ lopy/current/LGW/LoRaGateway.py
@@ -127,7 +127,9 @@ def registering_phase(msg):
     global isRegistered
     global slot
     #print("REGISTERING PHASE WITH "+str(msg.id_src)+" STARTED")
-    s.send('DataReq,'+str(4)+','+str(frequency)+','+str(slot)+','+str(id)+','+str(msg.id_src)+','+str(-1)+','+str(slot))
+    if msg.id_src in idRegistered:
+        s.send('DataReq,'+str(4)+','+str(frequency)+','+str(slot)+','+str(id)+','+str(msg.id_src)+','+str(-1)+','+str(slot))
+    
     if msg.id_src in isRegistered:
         print("Added before")
     else:
